01ff1e620dec3854836a47901c7606e5e3aa28d0
deardileep1998 <150879653+deardileep1998@users.noreply.github.com>
2023-12-07 15:16:56 -0500

    Update FieldInhabitant.py
    Added comments and Function docstrings

--- FieldInhabitant.py
+++ FieldInhabitant.py
@@ -1,13 +1,27 @@
-# FieldInhabitant.py
-
 class FieldInhabitant:
     def __init__(self, symbol):
+        """
+        Initializes a FieldInhabitant object.
+
+        :param symbol: The symbol representing the inhabitant on the field.
+        :type symbol: str
+        """
         self._symbol = symbol  # Protected member variable
 
-    # Getter for symbol
     def get_symbol(self):
+        """
+        Retrieves the symbol representing the FieldInhabitant.
+
+        :return: The symbol.
+        :rtype: str
+        """
         return self._symbol
 
-    # Setter for symbol
     def set_symbol(self, value):
+        """
+        Sets the symbol representing the FieldInhabitant.
+
+        :param value: The new symbol value.
+        :type value: str
+        """
         self._symbol = value
