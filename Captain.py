a28d59537c3002120bc15c209d9f08c04144fcb7
LokeshP79 <150841113+LokeshP79@users.noreply.github.com>
2023-12-07 15:13:54 -0500

    Update Captain.py
    Added comments and Function Docstring

--- Captain.py
+++ Captain.py
@@ -1,20 +1,36 @@
-# Captain.py
 from Creature import Creature
 
 class Captain(Creature):
     def __init__(self, x, y):
+        """
+        Initializes a Captain object.
+        
+        :param x: The x-coordinate of the Captain's position.
+        :type x: int
+        :param y: The y-coordinate of the Captain's position.
+        :type y: int
+        """
         super().__init__(x, y, "V")  # "V" is the symbol for the Captain
         self.__collected_veggies = []  # Private member variable for collected veggies
 
-    # Method to add a Veggie to the Captain's collection
     def addVeggie(self, veggie):
+        """
+        Adds a Veggie to the Captain's collection.
+
+        :param veggie: The Veggie to be added to the collection.
+        :type veggie: Veggie
+        """
         self.__collected_veggies.append(veggie)
 
-    # Getter for collected_veggies
     def get_collected_veggies(self):
+        """
+        Retrieves the list of collected veggies by the Captain.
+        :return: A list of collected veggies.
+        """
         return self.__collected_veggies
 
-    # There's no need for a setter for collected_veggies as we don't replace the whole list,
-    # but if you need to reset the list, you can use the following method
     def reset_collected_veggies(self):
+        """
+        Resets the list of collected veggies to an empty list.
+        """
         self.__collected_veggies = []
